acdcc9d8c491d1d972ea4dd9b0c039b201037eee
Harry Sild <46851868+Kypsis@users.noreply.github.com>
2023-04-10 16:39:46 +0300

    fix: [NO-TASK] Resize Icons story icons (#107)

--- example/lib/src/storybook/stories/icons.dart
+++ example/lib/src/storybook/stories/icons.dart
@@ -31,7 +31,7 @@ class IconsStory extends Story {
 
 class _IconsGridWithTitle extends StatelessWidget {
   final String title;
-  final List<Icon> iconsList;
+  final List<IconData> iconsList;
   final List<String> iconsNameList;
 
   const _IconsGridWithTitle({
@@ -56,8 +56,14 @@ class _IconsGridWithTitle extends StatelessWidget {
           itemBuilder: (context, index) {
             return Column(
               mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                iconsList[index],
+                if (iconsNameList[index].contains("16"))
+                  Icon(iconsList[index], size: 16)
+                else if (iconsNameList[index].contains("24"))
+                  Icon(iconsList[index], size: 24)
+                else
+                  Icon(iconsList[index], size: 32),
                 const SizedBox(height: 20),
                 Text(
                   iconsNameList[index],
