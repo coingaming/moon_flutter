ea7701912dea46dcbe76d5bdcdc2ced6ddb59b66
Kypsis <puhhbaer@hotmail.com>
2023-04-25 01:47:38 +0300

    Fix disappearing widgets

--- lib/src/utils/extensions.dart
+++ lib/src/utils/extensions.dart
@@ -14,22 +14,24 @@ extension DarkModeX on BuildContext {
 extension BorderRadiusX on BorderRadius {
   /// Returns SmoothBorderRadius.
   SmoothBorderRadius get smoothBorderRadius {
+    // FIXME: CornerSmoothing of 1 creates null pointer dereference error for some reason with SmoothRectangleBorder
+    // for some reason. So we use 0.999 instead.
     return SmoothBorderRadius.only(
       topLeft: SmoothRadius(
         cornerRadius: topLeft.x,
-        cornerSmoothing: 1,
+        cornerSmoothing: 0.999,
       ),
       topRight: SmoothRadius(
         cornerRadius: topRight.x,
-        cornerSmoothing: 1,
+        cornerSmoothing: 0.999,
       ),
       bottomLeft: SmoothRadius(
         cornerRadius: bottomLeft.x,
-        cornerSmoothing: 1,
+        cornerSmoothing: 0.999,
       ),
       bottomRight: SmoothRadius(
         cornerRadius: bottomRight.x,
-        cornerSmoothing: 1,
+        cornerSmoothing: 0.999,
       ),
     );
   }
