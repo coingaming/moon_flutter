0746660e04640cf9ed4bbee8294d98024f5b0a1c
Harry Sild <46851868+Kypsis@users.noreply.github.com>
2024-05-14 14:27:16 +0300

    Remove Tag uppercase from snippets

--- example/assets/code_snippets/dropdown.md
+++ example/assets/code_snippets/dropdown.md
@@ -77,7 +77,6 @@ class _DropdownState extends State<Dropdown> {
                 ? Center(
                     child: MoonTag(
                       tagSize: MoonTagSize.xs,
-                      isUpperCase: false,
                       backgroundColor: context.moonColors!.bulma,
                       onTap: () => setState(() => _availableChoices.updateAll((key, value) => false)),
                       label: Text(
--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import package_info_plus
 import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
